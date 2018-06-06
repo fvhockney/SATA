45a9fc0da98e789c35caf8d15eae48258fe8f1e5
Fred Hockney <fvhockney@gmail.com>
2018-06-06 20:14:11 +0200

    update to laravel 5.6; custom input footer;

--- resources/views/partials/mainFoot.blade.php
+++ resources/views/partials/mainFoot.blade.php
@@ -1,15 +1,25 @@
 <footer class="d-flex flex-column flex-md-row justify-content-around py-3 bg-dark text-light">
-  <div>
-    copyright 2018<br>
-    Fernweh Israel
-  </div>
-  <div>
-    Name<br>
-    Address<br>
-    email@email.com<br>
-    +55 155 555 5555
-  </div>
-  <div>
-    Foot Nav
-  </div>
+    <div>
+        &copy; 2018 Fernweh Israel<br>
+        P.O. Box 12345<br>
+        Trenton, NJ 57849<br>
+        general@fernweh.com<br>
+        (123)456-7890
+    </div>
+    <div>
+    <h6>Sign up for our Newsletter:</h6>
+        <form action="" method="">
+            <div class="input-group">
+                <input id="newsletter-input" type="text" class="form-control" name="newsletterSignUp" placeholder="Email">
+                <div  class="input-group-append">
+                    <button id="newsletter-btn">
+
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div>
+        Foot Nav
+    </div>
 </footer>
